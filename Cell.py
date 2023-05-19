4a3c8168d4f5a46ad365cd3ca780c4ee04ea56bc
Jaber Jaber <jaberabdulhalim@gmail.com>
2023-05-19 19:10:58 +1000

    Template sec, geo, phys & connectivity defined

--- Cell.py
+++ Cell.py
@@ -1,4 +1,46 @@
 from neuron import h
 
 class Cell():
-    print("Hello World")
+    # Cell template. All other cells in this model will inherit from Cell.
+
+    def __init__(self):
+
+        # Cell in 3D space
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+        # Defining the soma, array of synapses and "Iscale"
+        
+        self.soma = None
+        self.Syn_list = []
+        self.Iscale = 0
+
+        # Cell methods (equivalent of procs in HOC)
+        self.create_sections()
+        self.define_biophysics()
+        self.define_geometry()
+        self.build_subsets
+
+    def create_sections(self):
+        # Sections in the form of:
+        # h.Section(name='soma', cell=self)
+        # or h.Section(name='dendrite', cell=self)
+
+        
+        raise NotImplementedError("Sections have not been created.")
+
+    def define_geometry(self):
+        # Set the 3D geometry of the cell.
+        raise NotImplementedError("Cell geometries have not been created.")
+
+    def define_biophysics(self):
+        # Define biophysics of the cell (assign membrane properties)
+        raise NotImplementedError("Biophysics of the cell have not been defined.")
+
+    def self_con(self, threshold=10):
+        netcon = h.NetCon(self.soma(0.5)._ref_v, self.soma)
+        netcon.threshold = threshold
+        return netcon
+
+    
