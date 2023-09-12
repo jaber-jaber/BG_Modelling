14fe32b5ee3c91f3e7d8dfb4f0bc75ac38cdb6c9
Jaber Jaber <jaberabdulhalim@gmail.com>
2023-09-12 20:11:27 +1000

    Inhibitory connection test

--- set_network.py
+++ set_network.py
@@ -60,3 +60,16 @@ class Network():
             for gpe in channel[1:4]:
                 pass
 
+### Code to configure ###
+
+# synapses = [[None for _ in range(num_cells)] for _ in range(num_cells)]
+#     netcons = [[None for _ in range(num_cells)] for _ in range(num_cells)]
+
+#     for i in range(num_cells):
+#         for j in range(num_cells):
+#             if i != j:  # Don't connect a cell to itself
+#                 synapses[i][j] = h.ExpSyn(Cells_List[j](0.5))
+#                 netcons[i][j] = h.NetCon(Cells_List[i](0.5)._ref_v, synapses[i][j], sec=Cells_List[i])
+#                 netcons[i][j].weight[0] = 0.04
+#                 netcons[i][j].delay = 5
+#     ```
