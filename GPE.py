b1443f308b506d8dc92bc95335124d916aa96443
hrshdv2nd <hv_2001@outlook.com>
2023-09-13 22:09:39 +1000

    New GPE Values

--- GPE.py
+++ GPE.py
@@ -7,7 +7,7 @@ class GPE(Cell):
         self.soma = h.Section(name='soma', cell=self)
 
     def define_geometry(self):
-        self.soma.L = self.soma.diam = 22.5 # This value will depend on the experimental papers
+        self.soma.L = self.soma.diam = 60 # This value will depend on the experimental papers
         # 22.5 is for STN
         self.shape_3D()
         
@@ -33,19 +33,19 @@ class GPE(Cell):
 
         self.soma.insert('gpe')
         h("cai0_ca_ion = 5e-6") # Initial intracellular Ca concentration
-        h("cao0_ca_ion = 2") # Initial extracellular Ca concentration
+        h("cao0_ca_ion = 2") # Initial extracellular Ca concentration 
         
         # Rhythmic Spontaneous activity is primarily driven by
-        self.soma.gnabar_gpe = 49e-3 # Fast sodium channel
-        self.soma.gkdrbar_gpe = 57e-3 # Delayed rectifier K channel (repolarization)
+        self.soma.gnabar_gpe = 49e-3 # Fast sodium channel (GPE)
+        self.soma.gkdrbar_gpe = 57e-3 # Delayed rectifier K channel (repolarization) (GPE)
         self.soma.gkabar_gpe = 5e-3 # A-type potassium channel for delaying depolarization
 
         # Freq of spontaneous activity depends on
-        self.soma.gkcabar_gpe = 1e-3 # AHP Ca2+-dependent K channel, changed to Otsuka's.
+        self.soma.gkcabar_gpe = 1e-3 # AHP Ca2+-dependent K channel, changed to Otsuka's. (GPE)
         self.soma.gcalbar_gpe = 15e-3 # Long-lasting calcium Ca2+ currents
-        self.soma.gcatbar_gpe = 5e-3 # Low threshold T-type Ca2+ currents
+        self.soma.gcatbar_gpe = 5e-3 #Low threshold T-type Ca2+ currents (GPE)
 
         # Ca dynamics and leak current
-        self.soma.kca_gpe = 2 # Ca removal rate
-        self.soma.gl_gpe = 0.35e-3 # Leak current, changed to Otsuka's. Original value: 0.29e-3 S/cm^2
+        self.soma.kca_gpe = 2 # Ca removal rate (GPE)
+        self.soma.gl_gpe = 0.35e-3 # Leak current, changed to Otsuka's. Original value: 0.29e-3 S/cm^2 (GPE)
         
